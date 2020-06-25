afbca650a01d5bef89ecd6f458debcb4d052ba91
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-25 17:23:01 +0200

    added stock module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
+import { StockModule } from './stock/stock.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { LegalComponent } from './routes/legal/legal.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LayoutModule
+    LayoutModule,
+    StockModule
   ],
   providers: [],
   bootstrap: [AppComponent]
