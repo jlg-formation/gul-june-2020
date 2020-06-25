04403626493bfa07d7f09b2e2fda99746cdc89d1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-25 17:24:44 +0200

    added stock routes

--- src/app/stock/stock.module.ts
+++ src/app/stock/stock.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StockRoutingModule } from './stock-routing.module';
+import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListComponent, CreateComponent],
   imports: [
     CommonModule,
     StockRoutingModule
