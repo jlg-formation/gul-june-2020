1bcdf07853cb70a640ffafc4ba14e3ab6f841a60
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-26 10:57:09 +0200

    plug fields on form

--- src/app/stock/create/create.component.ts
+++ src/app/stock/create/create.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('Tournevis', [Validators.required]),
+    price: new FormControl(2.34, [Validators.required]),
+    qty: new FormControl(100, [Validators.required]),
+  });
 
-  f = new FormGroup({});
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
