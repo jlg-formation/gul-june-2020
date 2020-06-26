60cf9c99a8e28630a6894760383df925572a8386
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-26 16:16:49 +0200

    change locale and currency by default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, DEFAULT_CURRENCY_CODE, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,10 +13,22 @@ import { ArticleService } from './services/article.service';
 import { HttpArticleService } from './services/http-article.service';
 import { HttpClientModule } from '@angular/common/http';
 
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule, StockModule, HttpClientModule],
-  providers: [{ provide: ArticleService, useClass: HttpArticleService }],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+    StockModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: ArticleService, useClass: HttpArticleService },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+    { provide: LOCALE_ID, useValue: 'fr' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
