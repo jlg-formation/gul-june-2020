2f265395c069467a74e3e73c484d8bee9b213501
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-26 10:15:39 +0200

    show article from service

--- src/app/stock/list/list.component.ts
+++ src/app/stock/list/list.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  constructor(public articleService: ArticleService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
